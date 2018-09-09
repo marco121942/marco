87cc9468029bcebf5e27b4b8158e38c4ca59814f
marco121942 <marco.sotomayor@tecsup.edu.pe>
2018-09-09 02:01:54 -0500

    Maven

---
+++ exampleMavenProject/src/main/java/com/tecsup/maven/lab01/App.java
@@ -0,0 +1,14 @@
+package com.tecsup.maven.lab01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
